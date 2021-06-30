29e951db3dfe039ee25acf935aa0fc9640d1f450
NProlic <nikola.prolic@gmail.com>
2021-06-30 18:40:40 +0200

    Functional menu

--- js/js.js
+++ js/js.js
@@ -1,4 +1,17 @@
+/*Menu icon*/
+function iconTransform(x) {
+    x.classList.toggle("change");
+}
+
+var popup = false;
+var buttons = document.getElementsByClassName("button");
+
 /*Navigation control*/
 function openNav() {
-    document.getElementById("mobile").style.height = "30%";
+    if (!popup) {
+            document.getElementById("mobile").style.height = "30%";
+    } else {
+            document.getElementById("mobile").style.height = "0%";
+    }
+    popup = !popup;
 }
